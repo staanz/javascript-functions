6160b86c2317c15c82102409e9c25c051db3ced1
Shanthanu Varma <shanthanuvarma@Zeros-MacBook-Pro.local>
2021-02-13 23:23:44 -0500

    main test

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -119,6 +119,8 @@ const iterate = (state, iterations) => {
 };
 
 const main = (pattern, iterations) => {
+    const results = iterate(startPatterns[pattern], iterations);
+    results.forEach(r => console.log(printCells(r)));
 };
 
 const startPatterns = {
